c364c75798432a1e3dfaf3ce5c173edc28b14a42
JP Quicksall <john.paul.quicksall@gmail.com>
2015-11-05 16:46:31 -0800

    Recieves events properly now

--- listener.py
+++ listener.py
@@ -2,24 +2,23 @@ from sseclient import SSEClient
 from flask import Flask, url_for, request, jsonify
 import json, requests, atexit
 
-app = Flask(__name__)
-
 with open('config.json') as data_file:
   config = json.load(data_file)
-
-@app.route('/')
-def multiply(a, b):
-  return a*b
 
 messages = SSEClient('{0}/notifyr?access_token={1}'
                         .format(config["particle-url"],config["token"]))
 
 for msg in messages:
-  event = str(msg.event).encode('ascii', 'ignore').decode('ascii')
-  data = str(msg.data).encode('ascii', 'ignore').decode('ascii')
-  print event
-  print data
-#  dataJson = json.loads(data)
+  #event = str(msg.event).encode('ascii', 'ignore').decode('ascii')
+  #data = str(msg.data).encode('ascii', 'ignore').decode('ascii')
+  #print(event)
+  #print(data)
+  print(msg.event)
+  if msg.event == "notifyr/announce":
+    print(msg.data)
+    dataJson = json.loads(msg.data)
+    print(dataJson["data"])
 
-if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 8080)
+  if msg.event == "TestingTheNotifyrCode":
+    print("CONFIRMED")
+    
